fe36f40bc3a46c611299820ee9398c0525166f9c
Dabod <guepardar@gmail.com>
2021-10-27 19:25:25 +0200

    Segundo upload
    aaaaaaaaaaaaaaaaaaaaaaaa

--- Assets/Scripts/LogicaAreaPuntuacion.cs
+++ Assets/Scripts/LogicaAreaPuntuacion.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class LogicaAreaPuntuacion : MonoBehaviour
 {
+    public AudioSource puntSonido;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         LogicaPuntuacion.puntuacion++;
+        puntSonido.Play();
     }
 }
