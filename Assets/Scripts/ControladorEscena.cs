e6b963b88a3ae7ab321d98dd26b1858dcbe52d4a
Dabod <guepardar@gmail.com>
2021-10-29 20:13:08 +0200

    Afegit efecte parallax al fons
    + proves fade out al premer el botó play.

--- Assets/Scripts/ControladorEscena.cs
+++ Assets/Scripts/ControladorEscena.cs
@@ -11,7 +11,10 @@ public class ControladorEscena : MonoBehaviour
     public GameObject canvasScore;
     public GameObject canvasPerder;
     public GameObject botonPlay;
+    public GameObject squareFade;
     public AudioSource musica;
+    public Animator bckLyrAnim;
+    public Animator mdlLyrAnim;
     public Animator terrainAnim;
     public Animator birdAnim;
     public Sprite playSprite;
@@ -35,11 +38,18 @@ public class ControladorEscena : MonoBehaviour
         pj.GetComponent<SpriteRenderer>().enabled = true;
         Time.timeScale = 1;
         musica.Play();
+        //StartCoroutine(esperaNegro());
         canvasScore.SetActive(true);
         canvasMenu.SetActive(false);
         botonPlay.SetActive(false);
         btnPulsado = true;
     }
+    //IEnumerator esperaNegro()
+    //{
+    //    squareFade.GetComponent<SpriteRenderer>().color += new Color(0, 0, 0, 255);
+    //    yield return new WaitForSeconds(0.1f);
+    //    squareFade.GetComponent<SpriteRenderer>().color -= new Color(0, 0, 0, 255);
+    //}
 
     public void Perder()
     {
@@ -48,6 +58,8 @@ public class ControladorEscena : MonoBehaviour
         botonPlay.GetComponent<UnityEngine.UI.Image>().sprite = menuSprite;
         //btnSize.rect.width;
         //botonPlay.GetComponent<RectTransform>().sizeDelta()
+        bckLyrAnim.updateMode = UnityEngine.AnimatorUpdateMode.Normal;
+        mdlLyrAnim.updateMode = UnityEngine.AnimatorUpdateMode.Normal;
         terrainAnim.updateMode = UnityEngine.AnimatorUpdateMode.Normal;
         birdAnim.updateMode = UnityEngine.AnimatorUpdateMode.Normal;
         botonPlay.SetActive(true);
